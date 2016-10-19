c70fa93a0d53f2bf07eddb363470350b0de39961
Nikolay <nikolay@nikolays-macbook-pro-2.local>
2016-10-19 21:45:23 +0300

    Initial Commit

---
+++ Matchismo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Matchismo
+//
+//  Created by Nikolay on 19/10/2016.
+//  Copyright © 2016 GnomVelikan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Matchismo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Matchismo
+//
+//  Created by Nikolay on 19/10/2016.
+//  Copyright © 2016 GnomVelikan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
